9262e9375ff8f0afca17f7c5f33814391082acb9
Hilenn James <diamondunderpressure@gmail.com>
2021-05-19 16:02:44 +0200

    updated code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
 import { WebService } from './web.service';
 import { NgForm } from '@angular/forms';
 import { Injectable } from '@angular/core';
@@ -7,70 +7,66 @@ import { Injectable } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl:'./app.component.html',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
 
-@Injectable({
-  providedIn: 'any'
-})
-
 export class AppComponent {
-  constructor(private webService : WebService){}
+  values: any;
+  result: string;
+  constructor(private webService: WebService){
+    this.values = {
+      input : '',
+      input2 : '' ,
+      operator : ''
+    };
+    this.result = '';
+  }
      title = 'AngularApp';
-     
-
-  //Variables
-          values = {
-                    input : '',
-                    input2 : '' ,
-                    operator : ''
-                  } 
-          result = '';
  /*********************************** */
 
-    ngOnInit()
+    OnInit(): void
     {
       this.webService.connect();
     }
 
     /***************************************** */
-    onSubmit(form : NgForm)
+    onSubmit(form: NgForm): any
     {
-     //set form data   
-        this.values.input = form.value.input
-        this.values.input2 = form.value.input2
-
-            console.log(this.values)
-    
-       this.grabResults();
+        this.values.input = form.value.input;
+        this.values.input2 = form.value.input2;
+        console.log(this.values);
+        this.grabResults();
     }
-
+    /******************************************* */
+    reset(form: NgForm): void {
+      this.result = '';
+      this.values = {};
+      form.resetForm();
+    }
     /**************************************** */
-    //below function send the request to the webservice and gets the result
-    grabResults()
+    // below function send the request to the webservice and gets the result
+    grabResults(): any
     {
       this.webService.calculate(this.values)
       .subscribe(
-        (result => this.finalAns(result)  
-      ))
-      
-    };
-    
+        (result => this.finalAns(result)
+      ));
+
+    }
 
     /*********************************** */
-    //Result is assigned  to app component
-    finalAns(result: any){
-      console.log(result)  
-      return  this.result =result.message;
+    // Result is assigned  to app component
+    finalAns(result: any): number {
+      console.log(result);
+      return  this.result = result.message;
     }
-    
-    
+
+
     /****************************************** */
-    //Get the value of the button 
-    btnClicked(target : any)
+    btnClicked(target: any): void
     {
-      this.values.operator =target.target.innerHTML
+      this.values.operator = target.target.innerHTML;
     }
-    
+
 }
--- src/app/web.service.ts
+++ src/app/web.service.ts
@@ -1,40 +1,35 @@
 import { HttpClient, HttpHeaders, } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, } from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 @Injectable()
 export class WebService{
     public result = '';
 
-
-    httpHeaders= new HttpHeaders
+    httpHeaders = new HttpHeaders
     ({
-        "Access-Contro-Allow-Origin": "*", 
-        "Access-Control-Allow-Headers": "Origin, X-Requested-Width, Content-Type, Accept"
-    })
+        'Access-Contro-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-Width, Content-Type, Accept'
+    });
 
-    constructor(private http: HttpClient ){}  
+    constructor(private http: HttpClient ){}
 
-    connect(){
+    connect(): Observable<any>{
         return  this.http.get('http://localhost:4300/api/calculate');
     }
 
-    
-     
-    calculate(values : any){
- 
-        //pipe to send result back to app component
-      let observable =   this.http.post('http://localhost:4300/api/calculate', values ,{
-            observe:'body',
+    calculate(values: any): Observable<any>{
+
+        // pipe to send result back to app component
+      const observable =   this.http.post('http://localhost:4300/api/calculate', values , {
+            observe: 'body',
             reportProgress: true
         }).pipe(
-            map(( result) =>{  
-                console.log(result ) 
-                return result
+            map(( result) => {
+                return result;
 
-        }),)
-        return observable;
+        }), );
+      return observable;
         }
-
-
-} 
+}
